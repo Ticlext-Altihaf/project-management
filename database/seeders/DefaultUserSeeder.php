6ac62a36446f01232f158f722975f47ce448a8c9
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-04-01 06:53:47 +0200

    fix: Spatie permission migration

--- database/seeders/DefaultUserSeeder.php
+++ database/seeders/DefaultUserSeeder.php
@@ -18,8 +18,8 @@ class DefaultUserSeeder extends Seeder
         if (User::where('email', 'john.doe@helper.app')->count() == 0) {
             $user = User::create([
                 'name' => 'John DOE',
-                'email' => 'john.doe@helper.app',
-                'password' => bcrypt('Passw@rd'),
+                'email' => 'admin@localhost',
+                'password' => bcrypt('admin'),
                 'email_verified_at' => now()
             ]);
             $user->creation_token = null;
